e6bca59e7b786b541c41391fe0f3286534c59d15
Shubham Bhardwaj <shubhardwaj10@gmail.com>
2024-06-27 14:45:22 +0530

    feat: Update NavBar to include Expense Tracker logo and rearrange navigation links

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -15,22 +15,25 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 
 function NavBar() {
   return (
-    <div className="p-2 flex gap-2 mx-2">
-      <Link to="/" className="[&.active]:font-bold mx-4">
-        Home
+    <div className="p-2 flex justify-start gap-2 mx-2">
+      <Link to="/" className="[&.active]:font-bold mx-4 w-full">
+        <h1 className="text-2xl font-semibold">Expense Tracker</h1>
       </Link>
-      <Link to="/expenses" className="[&.active]:font-bold mx-4">
-        Expenses
-      </Link>
-      <Link to="/create-expense" className="[&.active]:font-bold mx-4">
-        Create
-      </Link>
-      <Link to="/profile" className="[&.active]:font-bold mx-4">
-        Profile
-      </Link>
-      <Link to="/about" className="[&.active]:font-bold mx-4">
-        About
-      </Link>
+
+      <div className="p-2 flex gap-2 mx-2">
+        <Link to="/expenses" className="[&.active]:font-bold mx-4">
+          Expenses
+        </Link>
+        <Link to="/create-expense" className="[&.active]:font-bold mx-4">
+          Create
+        </Link>
+        <Link to="/profile" className="[&.active]:font-bold mx-4">
+          Profile
+        </Link>
+        <Link to="/about" className="[&.active]:font-bold mx-4">
+          About
+        </Link>
+      </div>
     </div>
   );
 }
